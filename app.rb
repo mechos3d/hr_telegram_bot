2cf8b202198ddd6064312737794ea865a25db3be
Gorgolevskiy Pavel <p.gorgolevskiy@potok.digital>
2020-04-09 00:25:26 +0300

    Added telegram-bot-ruby gem

--- app.rb
+++ app.rb
@@ -1,12 +1,14 @@
-require 'time'
+require 'telegram/bot'
 
-class App
-  def call
-    loop do
-      puts " ---> #{Time.now}"
-      sleep(2)
+token = ENV['HR_TELEGRAM_BOT_TOKEN']
+
+Telegram::Bot::Client.run(token) do |bot|
+  bot.listen do |message|
+    case message.text
+    when '/start'
+      bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}")
+    when '/stop'
+      bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
     end
   end
 end
-
-App.new.call
